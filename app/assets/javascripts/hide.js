302e0cf2d9cfbf9ad31b8c28134a56d193978360
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-05 20:17:55 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,11 +1,12 @@
 let hider = document.getElementById('hide_this')
 //
-// function hideWhenClicked(e) {
+function hideWhenClicked(e) {
+    hider.click(console.log('clicked!!!'))//.css({'visibility':'hidden'})};
 //   // hider.click(hideButton);
 //   hider.click(function(){
 //     console.log('button was clicked')
 //   })
-// }
+}
 
 // function hideWhenClicked(event) {
 //   var $target = $(event.target);
