7c194a73b2962b973f3d70c2fb0a7f19d5c620e8
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-01-05 20:22:49 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,5 +1,15 @@
+
 function hideWhenClicked(event) {
-  $(event.target).hide()
+  //$(event.target).hide()
+  $(event.target).css({'visibility':'hidden'})};
+
 }
+
+// function hideWhenClicked(event) {
+//   var $target = $(event.target);
+//   $target.hide();
+// }
+
+//hider.click(console.log('clicked!!!'))/
 
 $('#hide_this').click(hideWhenClicked)
